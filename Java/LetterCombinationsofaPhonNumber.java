3a1639c191c2e64f4e1e8cc42b77f5a45feb0da1
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-07-22 13:54:43 -0700

    7.22

--- Java/LetterCombinationsofaPhonNumber.java
+++ Java/LetterCombinationsofaPhonNumber.java
@@ -14,28 +14,33 @@ public class LetterCombinationsofaPhonNumber {
 	        "wxyz", //9
 	    };
 	    
-	    public List<String> solution(String digits) {
+	public List<String> solution(String digits) {
 	        List<String> res = new ArrayList<String>();
 	        if(digits == null || digits.length() == 0)
 	            return res;
 	        helper(digits, 0, "", res); //backtracking!!!
 	        return res;
-	    }
-	    
-	    private void helper(String digits, int s, String comb, List<String> res){
+	}
+	
+	/**
+	 * @param s len of digit already added
+	 * @param comb temp string (current result)
+	 * @param final result
+	 */
+	private void helper(String digits, int s, String comb, List<String> res){
 	        if(s == digits.length()){ // all digits done, return
 	            res.add(comb);
 	            return;
 	        }
 	        String c = LETTERS[digits.charAt(s) - '0']; //get int index of digits !!!! Since digits is a string!!!
-	        for(int i = 0; i < c.length(); i++){
+	        for(int i = 0; i < c.length(); i++){ // i starts from 0
 	            helper(digits, s + 1, comb + c.charAt(i), res); // backtracking!!!
 	        }
 	    }
 	    
 	    public static void main(String[] args) {
 	    	LetterCombinationsofaPhonNumber l = new LetterCombinationsofaPhonNumber();
-	    	List<String> list = l.solution("22");
+	    	List<String> list = l.solution("222");
 	    	System.out.println(list);
 	    }
 }
