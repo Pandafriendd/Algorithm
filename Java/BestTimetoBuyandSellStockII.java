23bbf54710625624a71b73e06378e04f3f991ea2
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-08-03 00:49:51 -0700

    8.2

---
+++ Java/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,77 @@
+/*
+ Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+             Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Example 2:
+
+Input: [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+             Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+             engaging multiple transactions at the same time. You must sell before buying again.
+Example 3:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class BestTimetoBuyandSellStockII {
+	
+	// approach 1 brute force
+	// time: O(n^n), recursive function is called n^n times
+	// space: O(n), depth of recursion is n
+	public int maxProfit(int[] prices) {
+		return helper(prices, 0);
+	}
+	public int helper(int[] prices, int pos) {
+		if(pos >= prices.length)
+			return 0 ;
+		int max = 0;
+		for(int start = pos; start < prices.length; start++) {
+			int maxprofit = 0;
+			for(int i = start + 1; i < prices.length; i++) {
+				if(prices[start] < prices[i]) {
+					int curprofit = prices[i] - prices[start] + helper(prices, i + 1);
+					maxprofit = Math.max(maxprofit, curprofit);
+				}
+			}
+			max = Math.max(max, maxprofit);
+		}
+		return max;
+	}
+	
+	// approach 2 peak valley approach
+	// we need to consider every peak immediately following a valley to maximize the profit
+	// time: O(n) simple pass
+	// space: O(1)
+	public int maxProfit2(int[] prices) {
+		if(prices == null || prices.length < 2)
+			return 0;
+		
+		int i = 0;
+		int valley = prices[0];
+		int peak = prices[0];
+		int maxprofit = 0;
+		
+		while(i < prices.length - 1) {   // !
+			while(i < prices.length - 1 && prices[i] >= prices[i + 1]) { // find valley
+				i++;
+			}
+			valley = prices[i];
+			while(i < prices.length - 1 && prices[i] <= prices[i + 1]) { // find peak
+				i++;
+			}
+			peak = prices[i];
+			maxprofit += peak - valley;
+		}
+		
+		return maxprofit;
+	}
+}
