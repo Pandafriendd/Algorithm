fbc81e0a0cdd9a2e9352c26c7a2641cb0fd62152
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-08-07 00:03:57 -0700

    8.6

--- Java/GroupAnagrams.java
+++ Java/GroupAnagrams.java
@@ -1,3 +1,22 @@
+/*
+ Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+ */
+
+
 import java.util.*;
 
 public class GroupAnagrams {
@@ -6,25 +25,54 @@ public class GroupAnagrams {
 	public List<List<String>> Solution(String[] strs) {
 		//List<List<String>> res = new ArrayList<List<String>>();
 		if(strs == null || strs.length == 0)
-			return new ArrayList();
+			return new ArrayList<>();
 		
-		Map<String, List> ans = new HashMap<String, List>();
+		Map<String, List<String>> ans = new HashMap<String, List<String>> ();
 		for(String s : strs) {
 			char[] c = s.toCharArray();
 			Arrays.sort(c);
-			String key = String.valueOf(c);
+			String key = String.valueOf(c);  // !!
 			if(!ans.containsKey(key))
-				ans.put(key, new ArrayList());
+				ans.put(key, new ArrayList<>());
 			ans.get(key).add(s);
 		}
 		
 		
-		return new ArrayList(ans.values()); //!!!
+		return new ArrayList<>(ans.values()); //!!!
+	}
+	
+	/**
+     * Use map<String, Integer>
+     * Integer is initialized as the index, updated to -1 when the word is added to map to make sure that no duplicate situation happens
+     */
+	public List<String> Solution2(String[] strs){
+		List<String> res = new ArrayList<String>(); //!!
+		if(strs == null || strs.length == 0)
+			return new ArrayList<>();
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for(int i = 0; i < strs.length; i++) {
+			char[] c = strs[i].toCharArray();
+			Arrays.sort(c);
+			String key = String.valueOf(c);
+			if(map.containsKey(key)) {
+				res.add(strs[i]); //add this string
+				if(map.get(key) >= 0) { //key string not added
+					res.add(strs[map.get(key)]);
+					map.put(key, -1); //mark already added as -1
+				}
+			}
+			else
+				map.put(key, i); //first put sorted string and index
+		}
+		
+		return res;
 	}
 	
 	public static void main(String[] args) {
 		GroupAnagrams g = new GroupAnagrams();
-		String[] s = {"aab", "baa", "acc"};
+		String[] s = {"aab", "baa", "acc", "cca"};
+		//System.out.println(g.Solution(s).toString());
 		System.out.println(g.Solution(s));
 	}
 }
