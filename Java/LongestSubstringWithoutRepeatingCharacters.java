79d23e9fbe2f66cf18bf9b704a0ca630f3c58c08
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-08-09 11:11:11 -0700

    8.9

--- Java/LongestSubstringWithoutRepeatingCharacters.java
+++ Java/LongestSubstringWithoutRepeatingCharacters.java
@@ -53,7 +53,7 @@ Doing so until s[j] is already in the HashSet.
 At this point, we found the maximum size of substrings without duplicate characters start with index i. 
 If we do this for all i, we get our answer.
 
-time: O(n)  
+time: O(2n). In the worst case each character will be visited twice by i and j.  space: O(n)
      */
     
     public int lengthOfLongestSubstring2(String s) {
@@ -71,5 +71,38 @@ time: O(n)
             }
         }
         return ans;
+    }
+    
+    // approach 3: optimized sliding window
+    /*
+     * Instead of using a set to tell if a character exists or not, 
+     * we could define a mapping of the characters to its index. 
+     * Then we can skip the characters immediately when we found a repeated character.
+     * 
+     * The reason is that if s[j] have a duplicate in the range [i, j)[i,j) with index j', 
+     * we don't need to increase i little by little. We can skip all the elements in the range [i, j'] and let i to be j' + 1 directly.
+     */
+    /**
+     * Traverse the string
+     * Get current character
+     * Update start point
+     * Update max
+     * Put current char in map
+     * time: O(n) space:O(n)
+     */
+    public static int lengthOfLongestSubstring222(String s) {
+        if (s == null || s.length() == 0) return 0;
+        
+        Map<Character, Integer> map = new HashMap<Character, Integer>(); // current index of character
+        int start = 0;
+        int max = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            start = Math.max(start, (map.containsKey(c)) ? map.get(c) + 1 : 0); /*start point can be recent dup's next char or last start*/
+            max = Math.max(max, i - start + 1); /*if current str len is bigger then update*/
+            map.put(c, i); /*add char to map with index*/
+        }
+        return max;
     }
 }
