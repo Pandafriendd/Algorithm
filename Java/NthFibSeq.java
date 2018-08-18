af47e51ac46283e44276bd31584d3e4cd2aae0f9
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-08-17 23:48:35 -0700

    8.17

--- Java/NthFibSeq.java
+++ Java/NthFibSeq.java
@@ -65,9 +65,11 @@ public class NthFibSeq {
 	  * Using power of the matrix {{1,1},{1,0}} 
 	  * This another O(n) which relies on the fact that if we n times multiply the matrix M = {{1,1},{1,0}} to itself (in other words calculate power(M, n )), 
 	  * then we get the (n+1)th Fibonacci number as the element at row and column (0, 0) in the resultant matrix.
+	  * Time Complexity: O(n)
+	  * Extra Space: O(1)
 	  */
 	 static int fib4(int n)
-	    {
+	{
 	    int F[][] = new int[][]{{1,1},{1,0}};
 	    if (n == 0)
 	        return 0;
@@ -76,8 +78,8 @@ public class NthFibSeq {
 	    return F[0][0];
 	    }
 	      
-	    static void multiply(int F[][], int M[][])
-	    {
+	 static void multiply(int F[][], int M[][])
+	  {
 	    int x =  F[0][0]*M[0][0] + F[0][1]*M[1][0];
 	    int y =  F[0][0]*M[0][1] + F[0][1]*M[1][1];
 	    int z =  F[1][0]*M[0][0] + F[1][1]*M[1][0];
@@ -87,21 +89,62 @@ public class NthFibSeq {
 	    F[0][1] = y;
 	    F[1][0] = z;
 	    F[1][1] = w;
-	    }
+	  }
 	      
-	    /* Optimized version of power() in method 4 */
-	    static void power(int F[][], int n)
-	    {
-	    if( n == 0 || n == 1)
-	      return;
+	 /* Helper function that calculates F[][] raise to the power n and puts the result in F[][]
+	    Note that this function is designed only for fib() and won't work as general power function */
+	static void power(int F[][], int n)
+	{
+	    int i;
 	    int M[][] = new int[][]{{1,1},{1,0}};
-	      
-	    power(F, n/2);
-	    multiply(F, F);
-	      
-	    if (n%2 != 0)
-	       multiply(F, M);
-	    }
+	     
+	    // n - 1 times multiply the matrix to {{1,0},{0,1}}
+	    for (i = 2; i <= n; i++)
+	        multiply(F, M);
+	}
+	  
+	  /*
+	   * Optimized power of matrix {{1,1},{1,0}}
+	   * can be optimized to work in O(Logn) time complexity. We can do recursive multiplication to get power(M, n) in the prevous method 
+	   * Time Complexity: O(Logn) 
+	   * Extra Space: O(Logn) if we consider the function call stack size, otherwise O(1).
+	   */
+    static int fib5(int n)
+    {
+    int F[][] = new int[][]{{1,1},{1,0}};
+    if (n == 0)
+        return 0;
+    power(F, n-1);
+      
+    return F[0][0];
+    }
+      
+    static void multiply2(int F[][], int M[][])
+    {
+    int x =  F[0][0]*M[0][0] + F[0][1]*M[1][0];
+    int y =  F[0][0]*M[0][1] + F[0][1]*M[1][1];
+    int z =  F[1][0]*M[0][0] + F[1][1]*M[1][0];
+    int w =  F[1][0]*M[0][1] + F[1][1]*M[1][1];
+     
+    F[0][0] = x;
+    F[0][1] = y;
+    F[1][0] = z;
+    F[1][1] = w;
+    }
+      
+    /* Optimized version of power() */
+    static void power2(int F[][], int n)
+    {
+    if( n == 0 || n == 1)
+      return;
+    
+    int M[][] = new int[][]{{1,1},{1,0}};  
+    power2(F, n/2);
+    multiply2(F, F);
+      
+    if (n%2 != 0) // n is odd
+       multiply2(F, M);
+    }
 
 	
 	
