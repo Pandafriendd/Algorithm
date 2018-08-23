aa7ac9f3dca523837a66288536735dc1bd278d2b
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-08-23 16:51:32 -0400

    8.23

--- Java/ContiguousArray.java
+++ Java/ContiguousArray.java
@@ -34,4 +34,56 @@ public class ContiguousArray {
 		
 		return maxlen;
 	}
+	
+	/*
+	 app2: Using extra array: count = 0; encounter 1, then count++; encounter 0, then count--
+	 If at any moment, the count becomes zero, it implies that we've encountered equal number of zeros and ones from the beginning till the current index of the array
+	 another point to be noted is that if we encounter the same count twice while traversing the array, 
+	 it means that the number of zeros and ones are equal between the indices corresponding to the equal count values
+	 Another point to be noted is that if we keep a track of the indices corresponding to the same count values that lie farthest apart, 
+	 we can determine the size of the largest subarray with equal no. of zeros and ones easily.
+	 
+	 time:O(n)  space:O(2n)
+	 */
+	public int findMaxLength2(int[] nums) {
+		int[] arr = new int[2 * nums.length + 1];
+		Arrays.fill(arr, -2);
+		arr[nums.length] = -1;
+		int maxlen = 0, count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			count = count + (nums[i] == 1 ? 1 : -1);
+			if(arr[count + nums.length] >= -1) {
+				maxlen = Math.max(maxlen, i - arr[count + nums.length]);
+			}
+			else {
+				arr[count + nums.length] = i;
+			}
+		}
+		return maxlen;
+	}
+	
+	/*
+	 app3: hashmap
+	 we make use of a HashMap to store count ==> index 
+	 We make an entry for a count in the map whenever the count is encountered first, 
+	 and later on use the corresponding index to find the length of the largest subarray with equal no. of zeros and ones when the same count is encountered again.
+	 
+	 time: O(n)   space: O(n)
+	 */
+	public int findMaxlength3(int[] nums) {
+		HashMap<Integer, Integer> map = new HashMap<>();
+		int count = 0;
+		int maxlen = 0;
+		map.put(0, -1);  //!!!!!! dealing with the case that when there are only two elements and count = 0. also means if we haven't included any numbers in the given array, the number of 1s = the number of 0s, and the index we can get this key is -1.
+		for(int i = 0; i < nums.length; i++) {
+			count = count + (nums[i] == 1 ? 1 : -1);
+			if(map.containsKey(count)) {
+				maxlen = Math.max(maxlen, i - map.get(count));
+			}
+			else
+				map.put(count, i);
+		}
+		
+		return maxlen;
+	}
 }
