ff26828f34e961c5293ca7ca85e5817092cace4c
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-08-24 19:04:09 -0400

    8.24

--- Java/HashMapDesign.java
+++ Java/HashMapDesign.java
@@ -25,6 +25,8 @@ All keys and values will be in the range of [0, 1000000].
 The number of operations will be in the range of [1, 10000].
 Please do not use the built-in HashMap library.
  */
+
+import java.util.*;
 public class HashMapDesign {
 	
 	final Bucket[] buckets = new Bucket[10000];
@@ -90,6 +92,33 @@ public class HashMapDesign {
     		this.key = key;
     		this.val = val;
     	}
+    }
+}
+
+// using huge array
+class MyHashMap {
+    
+    int[] map;
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        map = new int[1000001];
+        Arrays.fill(map, -1);
+    }
+    
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        return map[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        map[key] = -1;
     }
 }
 
