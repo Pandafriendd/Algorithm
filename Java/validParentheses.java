78d694c6aabb26796f236f9db240d1f7a5548ae4
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-09-26 18:02:59 -0400

    9.26

--- Java/validParentheses.java
+++ Java/validParentheses.java
@@ -28,3 +28,38 @@ class Solution {
     }
     
 }
+
+// Input:"{[]}"
+class Solution {
+    public boolean isValid(String s) {
+        if(s == null || s.length() == 0)
+            return true;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        if(s.charAt(0) == ')' || s.charAt(0) == '}' || s.charAt(0) == ']')
+            return false;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(stack.isEmpty())
+                stack.push(s.charAt(i));
+            else {
+                if(stack.peek() == '(' && s.charAt(i) != ')' || 
+                   stack.peek() == '[' && s.charAt(i) != ']' || 
+                   stack.peek() == '{' && s.charAt(i) != '}') {
+                    return false;
+                }
+                else if(stack.peek() == '(' && s.charAt(i) == ')' || 
+                        stack.peek() == '[' && s.charAt(i) == ']' || 
+                        stack.peek() == '{' && s.charAt(i) == '}') {
+                    stack.pop();
+                }
+                else {
+                    stack.push(s.charAt(i));
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
