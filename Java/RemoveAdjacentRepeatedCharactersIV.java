d207eac38a86b2c60d913abe4f5fe31768362417
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-10-20 01:49:48 -0400

    10.19

--- Java/RemoveAdjacentRepeatedCharactersIV.java
+++ Java/RemoveAdjacentRepeatedCharactersIV.java
@@ -1,34 +1,42 @@
-// something WRONG !!!!
-public String deDup(String input) {
-	if (input.isEmpty()) {
-		return input;
-	}
 
-	Deque<Character> stack = new ArrayDeque<>(); // all processed chars that should be kept
-	int j = 1; // fast pionter
-	char[] array = input.toCharArray();
+import java.util.*;
 
-	stack.push(array[0]);
-	while (j < array.length) {
-		char c = array[j];
-		if (!stack.isEmpty() && c == stack.peek()) {
-			while (j < array.length && c == stack.peek()) {
+public class RemoveAdjacentRepeatedCharactersIV {
+	
+	public static String deDup(String input) {
+		if (input.isEmpty()) {
+			return input;
+		}
+
+		Deque<Character> stack = new ArrayDeque<>(); // all processed chars that should be kept
+		int j = 0; // fast pionter
+		char[] array = input.toCharArray();
+
+		while (j < array.length) {
+			if (!stack.isEmpty() && array[j] == stack.peek()) {
+				while (j < array.length && array[j] == stack.peek()) {
+					j++;
+				}
+				// now j == a.len || a[j] != stack.peek()
+				stack.pop();	
+			}
+			else { // stack is empty || a[j] != peek
+				stack.push(array[j]);
 				j++;
 			}
-			// now j == a.len || a[j] != stack.peek()
-			stack.pop();	
 		}
-		else { // stack is empty || a[j] != peek
-			stack.push(array[j]);
-			j++;
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < stack.size(); i++) {
+			sb.append(stack.pop());
 		}
-	}
+		sb.reverse();
 
-	StringBuilder sb = new StringBuilder();
-	for (int i = 0; i < stack.size(); i++) {
-		sb.append(stack.pop());
+		return sb.toString();
 	}
-	sb.reverse();
-
-	return sb.toString();
+	
+	
+	public static void main(String[] args) {
+		System.out.println(deDup("abbbccd"));
+	}
 }
