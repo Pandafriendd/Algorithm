f55452bc8152a195277b2359640f5a827a523d44
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-11-19 20:52:25 -0500

    11.19

--- Java/DictionaryWordI.java
+++ Java/DictionaryWordI.java
@@ -17,14 +17,36 @@ Word: ¡°robcatbob¡± return true since it can be composed by "rob", "cat", "bob"
 import java.util.*;
 public class DictionaryWordI {
 	/*
-	 DP: M[i] if from 0-th to i-1-th elements can composed by concatenating words from the dict
+	 DP: M[i] if from 0-th to i-1-th elements (first i elements) can composed by concatenating words from the dict
 	 base case: M[0]
-	 indiuction rule: M[i] = true, if there are exist at least one j (j < i), such that M[j] is true && substring(j + 1, i) is also true
+	 indiuction rule: M[i] = true, if there are exist at least one j (j < i), such that M[j] is true && substring(j, i) is also true
 	 					   = false, otherwise
+	 					   
+	 time: O(n^3) since we use string utils
 	 */
 	public boolean canBreak(String input, String[] dict) {
-		HashSet
+		HashSet<String> dictSet = new HashSet<>();
+		for (String s : dict) {
+			dictSet.add(s);
+		}
 		
+		boolean[] M = new boolean[input.length() + 1];  // meaningful i from 1 to input.length
+		for (int i = 1; i <= input.length(); i++) {
+			if (dictSet.contains(input.substring(0, i))) {  // no cut
+				M[i] = true;
+				continue;
+			}
+			
+			// otherwise, check every possible cuts
+			for (int j = 1; j < i; j++) {
+				if (M[j] == true && dictSet.contains(input.substring(j, i))) {
+					M[i] = true;
+					break;
+				}
+			}
+		}
+		
+		return M[input.length()];
 	}
 
 }
