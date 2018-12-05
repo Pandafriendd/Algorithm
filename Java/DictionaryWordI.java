b126adaecebb7a8d2f04875147dd8d125ed59b22
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-12-04 22:58:16 -0500

    12.4

--- Java/DictionaryWordI.java
+++ Java/DictionaryWordI.java
@@ -2,16 +2,13 @@
 Given a word and a dictionary, determine if it can be composed by concatenating words from the given dictionary.
 
 Assumptions
-
 The given word is not null and is not empty
 The given dictionary is not null and is not empty and all the words in the dictionary are not null or empty
+
 Examples
-
 Dictionary: {"bob", "cat", "rob"}
-
-Word: ¡°robob¡± return false
-
-Word: ¡°robcatbob¡± return true since it can be composed by "rob", "cat", "bob"
+Word: "robob" return false
+Word: "robcatbob" return true since it can be composed by "rob", "cat", "bob"
  */
 
 import java.util.*;
@@ -19,7 +16,7 @@ public class DictionaryWordI {
 	/*
 	 DP: M[i] if from 0-th to i-1-th elements (first i elements) can composed by concatenating words from the dict
 	 base case: M[0]
-	 indiuction rule: M[i] = true, if there are exist at least one j (j < i), such that M[j] is true && substring(j, i) is also true
+	 indiuction rule: M[i] = true, if there are exist at least one j (j < i), such that M[j] is true && substring(j, i) is also in the dict
 	 					   = false, otherwise
 	 					   
 	 time: O(n^3) since we use string utils
--- Java/Word_Break.java
+++ Java/Word_Break.java
@@ -17,13 +17,12 @@ public class Word_Break {
         }
         
         for (String word : wordDict) {
-        	if (s.length() - startIndex >= word.length()) {  // !!!!
-        		if (s.substring(startIndex, startIndex + word.length()).equals(word)) {
-        			if (wordBreakHelper(s, wordDict, startIndex + word.length()) == true) {  // !!!
-        				return true;
-        			}
+        	// startIndex + word.length() <= s.length()   // !!!!
+        	if (startIndex + word.length() <= s.length() && s.substring(startIndex, startIndex + word.length()).equals(word)) {
+        		if (wordBreakHelper(s, wordDict, startIndex + word.length()) == true) {  // !!!
+        			return true;
         		}
-            }
+        	}
         }
         
         return false;
@@ -31,39 +30,56 @@ public class Word_Break {
     
     
     public static boolean wordBreak2(String s, List<String> wordDict) {
-    	int[] M = new int[s.length()];
-        return wordBreakHelperNew(s, wordDict, 0, M);
+        return wordBreakHelperNew(s, wordDict, 0, new Boolean[s.length()]);
     }
     
     
-    // try to do memoization, 
+    // try to do memoization in Boolean array M, since Boolean initialize all elements to null
     // so time: O(m * n), since each char in s need to do only once calculation, each calculation iterate all words in wordDict
     // space: O(m + m)
-    private static boolean wordBreakHelperNew(String s, List<String> wordDict, int startIndex, int[] M) {
+    private static boolean wordBreakHelperNew(String s, List<String> wordDict, int startIndex, Boolean[] M) {
         // base case
         if (startIndex >= s.length()) {
             return true;
         }
         
-        if (M[startIndex] == 1) {
-        	return true;
-        } else if (M[startIndex] == -1) {
-        	return false;
+        if (M[startIndex] != null) {
+        	return M[startIndex];
         }
         
         for (String word : wordDict) {
-        	if (s.length() - startIndex >= word.length()) {  // !!!!
-        		if (s.substring(startIndex, startIndex + word.length()).equals(word)) {
-        			if (wordBreakHelperNew(s, wordDict, startIndex + word.length(), M) == true) {  // !!!
-        				M[startIndex] = 1;
-        				return true;
-        			}
+        	if (startIndex + word.length() <= s.length() && s.substring(startIndex, startIndex + word.length()).equals(word)) {
+        		if (wordBreakHelperNew(s, wordDict, startIndex + word.length(), M) == true) {  // !!!
+        			return M[startIndex] = true;
         		}
-            }
+        	}
+        }
+        return M[startIndex] = false;
+    }
+    
+    /*
+    bottom-up DP:
+    M[i] represents whether first i elements in s (from index 0 to i - 1) can be segmented into a sequence of one or more dictionary words.
+    base case:
+    M[0] = true?
+    induction rule:
+    M[i] = true if there exist at least one j (0 <= j < i) so that M[j] is true && substring(j, i) is in the wordDict (j = 0 means this substring doesn't need to cut)  // !!!!!!!!!!!!!
+         = false, otherwise
+     */
+    public static boolean wordBreak3(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        boolean[] M = new boolean[s.length() + 1];
+        M[0] = true;
+        for (int i = 1; i <= s.length(); i++) { // !!! <=
+        	for (int j = 0; j < i; j++) {
+        		if (M[j] && wordDictSet.contains(s.substring(j, i))) {
+        			M[i] = true;
+        			break;
+        		}
+        	}
         }
         
-        M[startIndex] = -1;
-        return false;
+        return M[s.length()];
     }
     
     
