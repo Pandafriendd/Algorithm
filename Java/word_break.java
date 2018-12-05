21ce550fc7075f40095d59a51d785c630cfadb12
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-12-04 19:27:21 -0500

    12.4

--- Java/word_break.java
+++ Java/word_break.java
@@ -29,6 +29,44 @@ public class word_break {
         return false;
     }
     
+    
+    public static boolean wordBreak2(String s, List<String> wordDict) {
+    	int[] M = new int[s.length()];
+        return wordBreakHelperNew(s, wordDict, 0, M);
+    }
+    
+    
+    // try to do memoization, 
+    // so time: O(m * n), since each char in s need to do only once calculation, each calculation iterate all words in wordDict
+    // space: O(m + m)
+    private static boolean wordBreakHelperNew(String s, List<String> wordDict, int startIndex, int[] M) {
+        // base case
+        if (startIndex >= s.length()) {
+            return true;
+        }
+        
+        if (M[startIndex] == 1) {
+        	return true;
+        } else if (M[startIndex] == -1) {
+        	return false;
+        }
+        
+        for (String word : wordDict) {
+        	if (s.length() - startIndex >= word.length()) {  // !!!!
+        		if (s.substring(startIndex, startIndex + word.length()).equals(word)) {
+        			if (wordBreakHelperNew(s, wordDict, startIndex + word.length(), M) == true) {  // !!!
+        				M[startIndex] = 1;
+        				return true;
+        			}
+        		}
+            }
+        }
+        
+        M[startIndex] = -1;
+        return false;
+    }
+    
+    
     public static void main(String[] args) {
     	List<String> list = new ArrayList<>();
     	list.add("car");
