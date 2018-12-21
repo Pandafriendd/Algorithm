3589f9498734a1035ad144f0d8306e8bbdd44cac
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-12-20 23:14:09 -0500

    12.20

---
+++ Java/CheckIfChained.java
@@ -0,0 +1,56 @@
+/*
+two string s1, s2 can be chained if the last letter of s1 is identical to the first letter of s2
+abc and cd can be chained
+abc and dz cannot be chained
+ */
+
+public class CheckIfChained {
+	/*
+	 based on the permutation, time: O(n! * n), space: O(n)
+	 */
+	public boolean isChained(String[] arr) {
+		if (arr == null || arr.length == 0) {
+			return false;
+		}
+		boolean[] res = new boolean[] {false};
+		findPermutation(arr, 0, res);
+		return res[0];
+	}
+	
+	private void findPermutation(String[] arr, int index, boolean[] res) {
+		// base case
+		if (index == arr.length) {
+			if (checkIfChain(arr)) {
+				res[0] = true;
+			}
+			return;
+		}
+		
+		// recursion rules
+		for (int i = index; i < arr.length; i++) {
+			swap(arr, index, i);
+			findPermutation(arr, index + 1, res);
+			swap(arr, index, i);
+		}
+	}
+	
+	private void swap(String[] arr, int i, int j) {
+		String t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
+	
+	private boolean checkIfChain(String[] arr) {
+		if (arr.length <= 1) {
+			return false;
+		}
+		
+		for (int i = 0; i <= arr.length - 2; i++) {
+			if (arr[i].charAt(arr[i].length() - 1) != arr[i + 1].charAt(0)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
