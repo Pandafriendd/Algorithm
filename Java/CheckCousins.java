4f41b1f5f82e7a102e56a5feac77778bd0367f77
Zhiyuan Li <lizhiyuan1995@outlook.com>
2018-12-25 16:15:56 -0500

    12.25

--- Java/CheckCousins.java
+++ Java/CheckCousins.java
@@ -1,5 +1,5 @@
 /*
-Given the binary Tree and the two nodes say ¡®a¡¯ and ¡®b¡¯, determine whether the two nodes are cousins of each other or not.
+Given the binary Tree and the two nodes say a and b, determine whether the two nodes are cousins of each other or not.
 Two nodes are cousins of each other if they are at same level and have different parents.
 
 Example:
