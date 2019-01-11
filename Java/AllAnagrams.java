9a6b736cb9fc716668f0824f6bb4e7cd67a953b3
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-01-10 22:09:40 -0500

    1.10.19

--- Java/AllAnagrams.java
+++ Java/AllAnagrams.java
@@ -1,4 +1,14 @@
+/*
+Find all occurrence of anagrams of a given string s in a given string l. Return the list of starting indices.
 
+Assumptions
+
+s is not null or empty.
+l is not null.
+Examples
+
+l = "abcbac", s = "ab", return [0, 3] since the substring with length 2 starting from index 0/3 are all anagrams of "ab" ("ab", "ba").
+*/
 
 import java.util.*;
 
