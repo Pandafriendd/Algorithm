55938cf302709ff331ddc1223c946bc0269dc459
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-01-13 02:17:30 -0500

    1.13.19

--- Java/AllAnagrams.java
+++ Java/AllAnagrams.java
@@ -128,6 +128,62 @@ public class AllAnagrams {
 	    return res;
 	}
 	
+	/*
+	use only one hashmap, representing how many chars are still needed in order to match s's anagram
+	time: same
+	space: O(m)
+	*/
+	public static List<Integer> allAnagrams(String s, String l) {
+		List<Integer> res = new ArrayList<>();
+		if (s.length() > l.length()) {
+			return res;
+		}
+		// the countMap represents how many chars are still needed in order to match s's anagram
+		Map<Character, Integer> countMap = new HashMap<>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);	
+			countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+		}
+		int numberOfMatch = 0;
+		int numberOfNeeded = 0;
+		int start = 0;
+		int end = 0;
+
+		while (end <= l.length()) {
+			if (numberOfMatch == countMap.size()) { // !!!
+				res.add(start);
+			}
+			if (end == l.length()) {  // !!!
+				break;
+			}
+
+			// move the window foward (remove the leftmost char and add the right most char)
+			// needed to move start pointer forward
+			if (end >= s.length()) {  // !
+				if (countMap.get(l.charAt(start)) != null) { 
+					numberOfNeeded = countMap.get(l.charAt(start));
+					numberOfNeeded++;
+					countMap.put(l.charAt(start), numberOfNeeded);
+					if (numberOfNeeded == 1) {
+						numberOfMatch--;
+					}
+				}
+				start++;
+			}
+			if (countMap.get(l.charAt(end)) != null) {
+				numberOfNeeded = countMap.get(l.charAt(end));
+				numberOfNeeded--;
+				countMap.put(l.charAt(end), numberOfNeeded);
+				if (numberOfNeeded == 0) {
+					numberOfMatch++;
+				}
+			}
+			end++;
+		}
+
+		return res;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(allAnagrams2("aab", "ababacbbaac"));
 	}
