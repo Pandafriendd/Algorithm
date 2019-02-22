6199624e6836372f8c31894b48f74886aeceb8d9
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-02-22 18:11:31 -0500

    2.22.19

--- Java/NQueensLOF.java
+++ Java/NQueensLOF.java
@@ -12,7 +12,9 @@ Each way is represented by a list of the Queen's y index for x indices of 0 to (
 Example
 N = 4, there are two ways of putting 4 queens:
 [1, 3, 0, 2] --> the Queen on the first row is at y index 1, 
-				 the Queen on the second row is at y index 3, 
+				 the Queen on the second ro
+				 
+				 w is at y index 3, 
 				 the Queen on the third row is at y index 0
 				 the Queen on the fourth row is at y index 2.
 [2, 0, 3, 1] --> the Queen on the first row is at y index 2, 
@@ -23,35 +25,36 @@ N = 4, there are two ways of putting 4 queens:
 
 public List<List<Integer>> nqueens(int n) {
 	List<List<Integer>> res = new ArrayList<>();
-	int[] queensPosition = new int[n];
+	List<Integer> queensPosition = new ArrayList<>();
 	nQueensHelper(res, queensPosition, 0, n);
 
 	return res;
 }
 
-private void nQueensHelper(List<List<Integer>> res, int[] queensPosition, int currentRow, int n) {
+private void nQueensHelper(List<List<Integer>> res, List<Integer> queensPosition, int currentRow, int n) {
 	// base case: last row is done, no row left
 	if (currentRow == n) {
-		res.add(Arrays.asList(queensPosition));
+		res.add(new ArrayList<>(queensPosition));
 		return;
 	}
 
 	// recursive rule: if the col is valid, go to next row (currentRow + 1)
 	for (int col = 0; col < n; col++) {
 		if (isValid(queensPosition, col, currentRow)) {
-			queensPosition[currentRow] = col;
+			queensPosition.add(col);
 			nQueensHelper(res, queensPosition, currentRow + 1, n);
+			queensPosition.remove(queensPosition.size() - 1);
 		}
 	}
 }
 
 // check if col, diagonals are valid. Since we put queens by row, don't need to check row
 // takes O(n) to check is valid
-private boolean isValid(int[] queensPosition, int col, int currentRow) {
-	for (int i = 0; i < queensPosition.length; i++) {
-		if (queensPosition[i] = col || col + currentRow == queensPosition[i] + i || col - currentRow == queensPosition[i] - i) {
+private boolean isValid(List<Integer> queensPosition, int col, int currentRow) {
+	for (int i = 0; i < queensPosition.size(); i++) {
+		if (queensPosition.get(i) == col || (col + currentRow == queensPosition.get(i) + i ) || (col - currentRow == queensPosition.get(i) - i)) {
 			return false;
 		}
 	}
 	return true;
-} 
+}  
