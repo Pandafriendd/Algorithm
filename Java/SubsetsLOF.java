9dcf247ed9eb8b93ed16190b38666e7782b0b2a5
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-02-23 16:52:55 -0500

    2.22.19

--- Java/SubsetsLOF.java
+++ Java/SubsetsLOF.java
@@ -28,13 +28,57 @@ public class Subset {
 	    subsetHelper(set, res, index + 1, sb);  // not pick the char at current index
 	    
 	  }
+	
+	
 	  
+	/* 
+eg: input a b c
+
+           abc
+     /      |    \
+   a(b c)  b(c)  c()
+   /   \     |
+ab(c)  ac()  bc()
+  |
+abc
+
+in this recursion tree, every node is a solution
+Cn1 + Cn2 + ... + Cnn, so time is O(2^n). space is O(n)
+*/
+public List<List<Integer>> subsets(int[] nums) {
+	List<List<Integer>> res = new ArrayList<>();
+	List<Integer> temp =  new ArrayList<>();
+	helper(res, temp, 0, nums);
+	return res;
+}
+
+private void helper(List<List<Integer>> res, List<Integer> temp, int index, int[] nums) {
+	res.add(new ArrayList<>(temp));
+
+	/* base case, atucally we don't need base case. we need to call res.add first
+	if (index == nums.length) {
+		return;
+	}
+	res.add(new ArrayList<>(temp));
+	*/
+	
+	for (int i = index; i < nums.length; i++) {
+		temp.add(nums[i]);
+		// helper(res, temp, index + 1, nums); !!!!! wrong!!!! i + 1
+		helper(res, temp, i + 1, nums);
+		temp.remove(temp.size() - 1);
+	}
+}
+	
+	
+	
+	
+	
 	  
 	  public static void main(String[] args) {
 		  System.out.println(Arrays.toString(subSets("abc").toArray()));
 	  }
-	  
-	  
+	    
 	  
 	  // using pure recursive
 	  public List<String> subSets111(String set) {
