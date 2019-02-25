c810d6a764927db233680ac6d7042b622e79d940
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-02-25 15:14:46 -0500

    2.25.19

--- Java/GenerateParenthesesII.java
+++ Java/GenerateParenthesesII.java
@@ -1,8 +1,9 @@
+
 import java.util.*;
 
-public class GenerateParenthesesII {
-	
-	/*
+
+
+/*
 	Get all valid permutations of l pairs of (), m pairs of <> and n pairs of {}.
 
 	Assumptions
@@ -11,14 +12,24 @@ public class GenerateParenthesesII {
 	Examples
 
 	l = 1, m = 1, n = 0, all the valid permutations are ["()<>", "(<>)", "<()>", "<>()"]
-	*/
+*/
 
+
+public class GenerateParenthesesII {
+	
+	
 	/*
 	[2,0,0]
 	Expected: [["(())","()()"]]
 	Your Solution: [["(())"]]
 	????
 	How to initialize the stack????? don't need to worry, just remember removing after adding
+	
+	brief of solution:
+	case 1: whenever we add a left p, check pRemain, push to stack, add it to sb
+	case 2: ....              right p, only need to check if it matches the top of stack
+		2.1: if matches: stack.pop, add it to sb
+		2.2 not matches: skip this branch, do not call recursion function
 	*/ 
 	public static List<String> validParentheses(int l, int m, int n) {
 		int[] pRemain = {l, m, n};
@@ -92,6 +103,16 @@ public class GenerateParenthesesII {
 			stack.push('{');
 		}
 	}
+	
+	/*
+	following up: we have an additional priority
+	
+	brief of solution:
+	case 1: whenever we add a left p, first check the priority of curr against the top of stack and pRmain, if valid, then push to stack, add it to sb
+	(same) case 2: ....      right p, only need to check if it matches the top of stack
+		2.1: if matches: stack.pop, add it to sb
+		2.2 not matches: skip this branch, do not call recursion function
+	 */
 	
 	public static void main(String[] args) {
 		System.out.println(validParentheses(2, 0 ,0));
