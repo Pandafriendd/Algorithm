fd9b30b8934ba2075df7e012af5e917b28f954b3
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-03-24 23:55:32 -0400

    3.24.19

--- Java/letter-combinations-of-a-phone-number.java
+++ Java/letter-combinations-of-a-phone-number.java
@@ -28,4 +28,37 @@ class Solution {
             sb.deleteCharAt(sb.length() - 1);
         }
     }
+    
+    // old old
+    private static final String[] LETTERS = {
+        "", //0
+        "", //1
+        "abc", //2
+        "def", //3;
+        "ghi", //4
+        "jkl", //5
+        "mno", //6
+        "pqrs", //7
+        "tuv", //8
+        "wxyz", //9
+    };
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<String>();
+        if(digits == null || digits.length() == 0)
+            return res;
+        helper(digits, 0, "", res); //backtracking!!!
+        return res;
+    }
+    
+    private void helper(String digits, int s, String comb, List<String> res){
+        if(s == digits.length()){ // all digits done, return
+            res.add(comb);
+            return;
+        }
+        String c = LETTERS[digits.charAt(s) - '0']; //get int index!!!! Since digits is a string!!!
+        for(int i = 0; i < c.length(); i++){
+            helper(digits, s + 1, comb + c.charAt(i), res); // backtracking!!!
+        }
+    }
 }
