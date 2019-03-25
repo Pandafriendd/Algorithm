bd9736a42ff7c9ed406547a47e8753b7cd538c97
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-03-25 18:31:01 -0400

    3.25.19

--- Java/letter-combinations-of-a-phone-number.java
+++ Java/letter-combinations-of-a-phone-number.java
@@ -20,7 +20,7 @@ class Solution {
         }
         
         // recursive rule
-        int digit = Character.getNumericValue(digits.charAt(index)); // digits.charAt(index) - '0';
+        int digit = Character.getNumericValue(digits.charAt(index)); // digits.charAt(index) - '0'; would be better
         String letters = digitToLetters[digit];
         for (int i = 0; i < letters.length(); i++) {
             sb.append(letters.charAt(i));
