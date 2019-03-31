53ccda16425f5823c986ce28a456dda617fd1597
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-03-30 23:35:08 -0400

    3.30.19

--- Java/RestoreIPAddresses.java
+++ Java/RestoreIPAddresses.java
@@ -147,3 +147,68 @@ public class RestoreIPAddresses {
     	System.out.println(obj.restoreIpAddresses("0279245587303"));
     }
 }
+
+// time: there is not more than 27 combinations to check.
+// space: constant space to keep the solutions, not more than 19 valid IP addresses.
+class Solution {
+  int n;
+  String s;
+  LinkedList<String> segments = new LinkedList<String>();
+  ArrayList<String> output = new ArrayList<String>();
+
+  public boolean valid(String segment) {
+    /*
+    Check if the current segment is valid :
+    1. less or equal to 255      
+    2. the first character could be '0' 
+    only if the segment is equal to '0'
+    */
+    int m = segment.length();
+    if (m > 3)
+      return false;
+    return (segment.charAt(0) != '0') ? (Integer.valueOf(segment) <= 255) : (m == 1);
+  }
+
+  public void update_output(int curr_pos) {
+    /*
+    Append the current list of segments 
+    to the list of solutions
+    */
+    String segment = s.substring(curr_pos + 1, n);
+    if (valid(segment)) {
+      segments.add(segment);
+      output.add(String.join(".", segments));
+      segments.removeLast();
+    }
+  }
+
+  public void backtrack(int prev_pos, int dots) {
+    /*
+    prev_pos : the position of the previously placed dot
+    dots : number of dots to place
+    */
+    // The current dot curr_pos could be placed 
+    // in a range from prev_pos + 1 to prev_pos + 4.
+    // The dot couldn't be placed 
+    // after the last character in the string.
+    int max_pos = Math.min(n - 1, prev_pos + 4);
+    for (int curr_pos = prev_pos + 1; curr_pos < max_pos; curr_pos++) {
+      String segment = s.substring(prev_pos + 1, curr_pos + 1);
+      if (valid(segment)) {
+        segments.add(segment);  // place dot
+        if (dots - 1 == 0)      // if all 3 dots are placed
+          update_output(curr_pos);  // add the solution to output
+        else
+          backtrack(curr_pos, dots - 1);  // continue to place dots
+        segments.removeLast();  // remove the last placed dot 
+      }
+    }
+  }
+
+  public List<String> restoreIpAddresses(String s) {
+    n = s.length();
+    this.s = s;
+    backtrack(-1, 3);
+    return output;
+  }
+}
