7e070c96de12432c0970145cd6d87e8a5fbcbe81
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-04-12 20:35:51 -0400

    4.12.19

--- Java/RemoveDuplicate.java
+++ Java/RemoveDuplicate.java
@@ -1,7 +1,7 @@
 
 /*
 Given a sorted array, remove all duplicated elements
-eg. 11223 -> 123
+eg. 1,1,2,2,3 -> 1,2,3
  */
 
 import java.util.*;
@@ -47,10 +47,12 @@ public class RemoveDuplicate {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString(removeDup(new int[] {1,2,3,3})));
+		System.out.println(Arrays.toString(removeAllDup(new int[] {1,1,1,2,2,3})));
 	}
 	
 	/*
+	Follow-up no.1
+	
 	Given a sorted array, remove all more than two duplicated elements so that only keep at most two duplicates
 	
 	slow: all elements to the left side of the slow (excluding slow) are the results of the elements have been processed
@@ -62,4 +64,40 @@ public class RemoveDuplicate {
 	case 1: input[fast] != input[slow - 2]: (either the first unique element or the second duplicate), so keep the current: input[slow] = input[fast]; slow++; fast++;
 	case 2: input[fast] == input[slow - 2]: (guarantee input[fast] == input[slow - 1]), so ignore the current: fast++;
 	 */
+	
+	/*
+	Follow-up no.2
+	
+	Given a sorted array, remove all duplicated elements so that only non-duplicated elements left
+	eg. 1,1,1,2,2,3 -> 3
+	
+	slow: all elements to the left side of the slow (excluding slow) are the results of the elements have been processed
+	fast: the current index being processed. All elements to the right side of the fast (including fast) are the elements have not been processed
+	beginFlag: a helper pointer to record the potential duplicate element
+	
+	initialize:
+	slow = 0, fast = 0
+	 */
+	public static int[] removeAllDup(int[] input) {
+		if (input.length <= 1 || input == null) {
+			return input;
+		}
+		
+		int slow = 0;
+		int fast = 0;
+		int beginFlag;
+		
+		while (fast < input.length) {
+			beginFlag = fast; // !!!
+			while (fast < input.length && input[fast] == input[beginFlag]) {
+				fast++;
+			}
+			// now fast points to the first element after beginFlag that doesn't equal to input[beginFlag]
+			if (fast - beginFlag == 1) {
+				input[slow++] = input[beginFlag];
+			}
+		}
+		
+		return reSize(input, slow);
+	}
 }
