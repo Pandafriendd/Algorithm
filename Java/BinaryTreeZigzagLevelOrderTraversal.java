1881b77c627f73b1a64ffac8455c6749d6bc61fd
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-04-15 01:28:43 -0400

    4.15.19

--- Java/BinaryTreeZigzagLevelOrderTraversal.java
+++ Java/BinaryTreeZigzagLevelOrderTraversal.java
@@ -19,5 +19,63 @@ return its zigzag level order traversal as:
 import java.util.*;
 
 public class BinaryTreeZigzagLevelOrderTraversal {
-
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+	
+	/*
+	 level from 0
+	 Initialize:
+	 deque.add(root)  Deque: head --- tail
+	 
+	 For each step:
+	 Case 1 even levels: Expend node from head to tail. Generate node.left to the tail, and then generate node.right to the tail !!!
+	 Case 2 odd levels: Expend node from tail to head. Generate node.right to the head, and then generate node.left to the head !!!
+	 */
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> res = new ArrayList<>();
+		if (root == null) {
+			return res;
+		}
+		
+		Deque<TreeNode> deque = new ArrayDeque<>();
+		deque.offerFirst(root);
+		int level = 0;
+		
+		while (!deque.isEmpty()) {
+			int size = deque.size();
+			List<Integer> list = new ArrayList<>();
+			while (size > 0) {
+				if (level % 2 == 0) { // even levels
+					TreeNode node = deque.pollFirst();
+					list.add(node.val);
+					if (node.left != null) {
+						deque.offerLast(node.left);
+					}
+					if (node.right != null) {
+						deque.offerLast(node.right);
+					}
+				} else { // odd levels
+					TreeNode node = deque.pollLast();
+					list.add(node.val);
+					if (node.right != null) {
+						deque.offerFirst(node.right);
+					}
+					if (node.left != null) {
+						deque.offerFirst(node.left);
+					}
+				}
+				size--;
+			}
+			res.add(list);
+			level++;  // !!
+		}
+		
+		return res;
+    }
 }
