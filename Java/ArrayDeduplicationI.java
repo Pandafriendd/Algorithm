0e0fc28748b50a64ee9a624df8a409c78f90a1fc
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-06-07 16:05:02 -0400

    6.7

---
+++ Java/ArrayDeduplicationI.java
@@ -0,0 +1,29 @@
+public class Solution {
+  public static int[] dedup(int[] input) {
+   if (input.length <= 1 || input == null) {
+			return input;
+		}
+		
+		int slow = 1;
+		int fast = 1;
+		
+		while (fast < input.length) {
+			if (input[fast] != input[slow - 1]) {
+				input[slow++] = input[fast++];
+			} else {
+				fast++;
+			}
+		}
+		
+		return reSize(input, slow);
+	}
+	
+	private static int[] reSize(int[] input, int slow) {
+		int[] output = new int[slow];
+		for (int i = 0; i < slow; i++) {
+			output[i] = input[i];
+		}
+		
+		return output;
+	}
+}
