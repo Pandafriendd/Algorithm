d5be07ff5895f812e32959c53c08f5bae034cc44
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-06-09 20:56:39 -0400

    6.9.19

--- Java/234Sum.java
+++ Java/234Sum.java
@@ -12,7 +12,7 @@ public boolean existSum(int[] array, int target) {
   }
  
 // return pairs
-public List<List<Integer>> allPairs(int[] array, int target) {  // assume no duplicates
+public List<List<Integer>> allPairs(int[] array, int target) {  // assume no duplicate
     Map<Integer, Integer> map = new HashMap<>();  // array[i] -> i
     List<List<Integer>> res = new ArrayList<>();
     for (int i = 0; i < array.length; i++) {
@@ -21,6 +21,25 @@ public List<List<Integer>> allPairs(int[] array, int target) {  // assume no dup
         res.add(Arrays.asList(map.get(newTarget), i));
       }
       map.put(array[i], i);
+    }
+    return res;
+  }
+
+public List<List<Integer>> allPairs(int[] array, int target) {  // assume have duplicates
+    Map<Integer, ArrayList<Integer>> map = new HashMap<>();  // array value -> list of all possible indeces
+    List<List<Integer>> res = new ArrayList<>();
+    for (int i = 0; i < array.length; i++) {
+      int newTarget = target - array[i];
+      List<Integer> indeces = map.get(newTarget);
+      if (indeces != null) { // if hit in the map
+        for (int index : indeces) {
+          res.add(Arrays.asList(index, i));
+        }
+      } 
+      if (!map.containsKey(array[i])) {
+        map.push(array[i], new ArrayList<Integer>());
+      }
+      map.get(array[i]).add(i);
     }
     return res;
   }
