00578faab1e0eb2bee83b8bf189bb7f65ba86602
Zhiyuan Li <lizhiyuan1995@outlook.com>
2019-08-25 17:33:50 -0400

    8/25/19

---
+++ Java/IntegerZip.java
@@ -0,0 +1,78 @@
+/*
+The integer zip of two non-negative integers A and B is an integer C whose decimal
+representation is created from the decimal representations of A and B as follows:
+the first (i.e. the most significant) digit of C is the first digit of A;
+the second digit of C is the first digit of B;
+the third digit of C is the second digit of A;
+the fourth digit of C is the second digit of B; etc.
+If one of the integers A and B runs out of digits, the remaining digits of the other integer are
+appended to the result. The decimal representation of 0 is assumed to be "0".
+Write a function:
+int solution(int A, int B)
+which, given two non-negative integers A and B, returns their integer zip. The function should
+return −1 if the result exceeds 100,000,000.
+Examples:
+solution(12, 34) returns 1324
+solution(12345, 678) returns 16273845
+solution(1234, 0) returns 10234
+Assumptions / constraints:
+A and B are integers within the range [0..100,000,000].
+ */
+import java.util.*;
+
+public class IntegerZip {
+	public static int IntegerZip(int a, int b) {
+		if (a > 1000000000 || b > 1000000000) {
+			return -1;
+		}
+
+		List<Integer> res = new ArrayList<>();
+		int dividandA = 1000000000;
+		int dividandB = 1000000000;
+		boolean isADone = false;
+		boolean isBDone = false;
+		boolean isDividandADone = false;
+		boolean isDividandBDone = false;
+		
+		while (!isADone || !isBDone) {
+			while (dividandA != 0 && a / dividandA == 0 && a != 0 && !isDividandADone) {
+				 dividandA = dividandA / 10;
+			}
+			isDividandADone = true;
+			if (isADone == false) {
+				res.add(a / dividandA);
+				a = a % dividandA;
+				dividandA = dividandA / 10;
+				if (a == 0) {
+					isADone = true;
+				}
+			}
+			
+			while (dividandB != 0 && b / dividandB == 0 && b != 0 && !isDividandBDone) {
+				dividandB = dividandB / 10;
+			}
+			isDividandBDone = true;
+			if (isBDone == false) {
+				res.add(b / dividandB);
+				b = b % dividandB;
+				dividandB = dividandB / 10;
+				if (b == 0) {
+					isBDone = true;
+				}
+			}
+		}
+
+		int resInt = 0;
+		int temp = 1;
+		for (int i = res.size() - 1; i >= 0; i--) {
+			resInt += res.get(i) * temp;
+			temp = temp * 10;
+		}
+
+		return resInt;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(IntegerZip(0,123));
+	}
+}
